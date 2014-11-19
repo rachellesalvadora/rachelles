8c94a50b6c9bb453239f6c81a39520d083a89f29
Rachelle Salvadora <rachellesalvadora@gmail.com>
2014-11-19 12:15:12 +1100

    index.php added:

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0">
+
+	<title>Rachelle Salvadora</title> 
+	<link rel="stylesheet" href="css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+	<div class="grid">
+
+		<div class="grid__item one-whole bg-1" >
+			<div class="wrap top-30">
+
+				<ul class="nav nav--banner nav-top">
+					<li class="nav-top-li"><a href="#about">About</a></li>
+					<li class="nav-top-li"><a href="#mypartner">My Partner</a></li>
+					<li class="nav-top-li"><a href="#philippines">Philippines Holiday</a></li>
+					<li class="nav-top-li"><a href="#singapore">Singapore Holiday</a></li>
+					<li class="nav-top-li"><a href="#contact">Contact</a></li>
+					<li class="nav-top-li"><em><a href="http://blog.rachellesalvadora.com" target="_blank">My Blog</a></em></li>
+				</ul>
+
+				
+				<ul class="nav-rachelle">
+					<li class="nav-top-li top-rachelle"><a href="#cs50" class="top-rachelle-a"></a></li>
+				</ul>
+
+
+							</div>
+			
+			<img src="img/bg-rach.jpg" class="bg-rach-img"/>
+			
+				<!--
+				<img class="logo" src="img/logo.png" alt="manak" />
+				-->
+
+				
+
+		</div>
+
+
+		<div class="grid__item one-whole bg-2">
+
+			<div id="about" class="container top-100">
+				<h1 class="h1-rachelle">Hi, I'm Rachelle Salvadora.</h1>
+				<p class="grid__item one-quarter">
+				You can view my certified certificate here:
+				<br/><br/>
+				<img  class="cs50-img" src="img/cs50-certificate.png" alt="CS50 Certificate" />
+				<br/><br/>
+				<a href="https://os.dce.harvard.edu/dce-public/certificate/verify/4c12a86a-341c-4d0b-bba4-db6385551aef">Verified link</a>
+			</p>
+
+
+				<p class="p-rachelle" class="grid__item two-thirds">
+					I'm from Taguig, Philippines, Not too long ago I was studying a Bachelor of Arts.<br/><br/>
+					I love <span>art and design</span> but when I learn <span>web development</span>, I challenged myself to express art in code. It was <span>HTML and CSS</span> at first. Then I realize there's so much more to it. With my eagerness to learn it deeply I took different courses from variety of online school around the internet. I learn something new from each of them but the most rewarding was <span>Harvard's CS50 Intro to Computer Science</span>.
+				</br></br>
+					Check the link on the right. It has the Signature of the Senior lecturer on Computer Science of Harvard University, <a href="http://cs.harvard.edu/malan">David Malan</a> and Huntington D. Lambert, Dean of Harvard Extension School.
+					<br/><br/>
+					I've Learnt a few CMS, Languages, Frameworks, Extensions,  Command line, Git, Database and so many other stuff that I've never known before.<strong></strong><br/>
+					<strong>I'm having a lot of fun </strong>playing around the tools and techniques that I see around around the internet towards front end web development.<br/>
+				</br>
+					
+
+					
+
+				<br/><br/>
+
+				
+			</p>
+		</div>
+
+	</div>
+
+
+
+	<div class="grid__item one-whole bg-4">
+		<div id="mypartner" class="container top-60 partner">
+			<div class="grid__item one-quarter">
+				<img src="img/gandr2.png" alt="g and r"/>
+			</div>
+			<div class="grid__item two-thirds">
+				<h1>My Partner</h2>
+					<p><span>His name is <a href="http://giolliano.com">Giolliano Sulit</a>, he's my other half.</span><br/><br/>
+						He is a web developer in melbourne. <br/>
+						Yup, I know what you're thinking. He is like the <strong>wizard </strong> and I'm the <strong>apprentice</strong>.<strong> </strong><br/><br/>
+						He is my boyfriend, partner, lover, tutor, and best friend. We've got a <a href="http://giollianoandrachelle.com">blog</a> together, you can read more about us there.<br/><br/>
+						Except from the internet, I leant most of what I know about web development from <em>him</em>.<br/><br/>
+						<strong></strong>
+					</p>
+				</div>
+			</div>
+		</div>
+
+
+
+
